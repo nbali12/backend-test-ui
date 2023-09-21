763a0535d8278c425649ae5efc4b04423e5ffd6e
nbali12 <BaliNishant@JohnDeere.com>
2023-09-21 19:33:12 -0400

    adding picking team logic

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [selectedDepartment, setSelectedDepartment] = useState();
+  const [selectedDepartment, setSelectedDepartment] = useState("");
   const [availableDates, setAvailableDates] = useState([]);
   const [selectedDate, setSelectedDate] = useState();
-  const [departmentData, setDepartmentData] = useState();
+  const [departmentData, setDepartmentData] = useState([]);
   // const [locationUpdates, setLocationUpdates] = useState([]);
 
   const Departments = ["Picking Team","Packing Team"]
@@ -15,56 +15,68 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`${baseUrl}\\order-dates`);
-        console.log('response')
-        console.log(response)
-        setAvailableDates(Array.from(new Set(response.data)));
+        const orderDates = await axios.get(`${baseUrl}/orderDates`);
+        setAvailableDates(Array.from(new Set(orderDates.data)));
       } catch (error) {
         console.error('Error fetching Route Session IDs:', error);
       }
     };
     fetchData();
   }, []);
-  
-  // useEffect(() => {
-  //   const fetchData = async () => {
 
-  //     if (selectedDate !== null) {
-  //       try {
-  //         const response = await axios.get(`${baseUrl}?route_session_id=${selectedRouteSessionId}`);
-  //         setLocationUpdates(response.data.map((coordinate) => {
-  //           return { latitude: coordinate[0], longitude: coordinate[1] };
-  //         }));
-  //       } catch (error) {
-  //         console.error('Axios error:', error);
-  //       }
-  //     }
-  //   };
-  //   fetchData();
-  // }, [selectedDate]);
-  
-
-  const handleDepartmentChange = (event) => {
-    const value = event.target.value;
-    console.log(value)
-    setSelectedDepartment(value);
-  };
+    const handleDepartmentChange = (event) => {
+      const value = event.target.value;
+      console.log(value)
+      setDepartmentData()
+      setSelectedDepartment(value);
+    };
 
     const handleDateChange = (event) => {
-    const value = event.target.value;
-    console.log(value)
-    setSelectedDate(value);
+      const value = event.target.value;
+      console.log(value)
+      setDepartmentData()
+      setSelectedDate(value);
     };
   
+  const handleGetDataClick = () => {
+    console.log("inside button");
+      getOrderData(selectedDepartment, selectedDate);
+    };
   
-  const getData = (selectedDepartment, selectedDate) => {
-    if (selectedDepartment === "Picking Team") {
-        
-    }
-    if (selectedDepartment === "Packing Team") {
-        
+  const getOrderData = async (selectedDepartment, selectedDate) => {
+      console.log("inside getOrderData");
+      let response;
+      if (selectedDepartment === "Picking Team") {
+        response = await axios.get(`${baseUrl}/lineItemCount?orderDate=${selectedDate}`)
+        console.log(response.data.lineItemCounts)
+        getPickerList(response.data.lineItemCounts)
       }
-  };
+      else if (selectedDepartment === "Packing Team") {
+        
+      console.log("inside getOrderData else if");
+        response = await axios.get(`${baseUrl}/orders?orderDate=${selectedDate}`)
+        console.log(response.data)
+      }
+      // setDepartmentData(response.data);
+    };
+  
+  const getPickerList = (data) => {
+
+    const itemToSell = [];
+    itemToSell.push(`Red Roses Bouquet x${data.ValentinesBox}`)
+    itemToSell.push(`Box of chocolates x${data.ValentinesBox}`)
+    itemToSell.push(`Love card x${data.ValentinesBox}`)
+    itemToSell.push(`Womens perfume x${data.ValentinesBox}`)
+    itemToSell.push(`Birthday cupcake x${data.BirthdayBox}`)
+    itemToSell.push(`$100 Visa Gift Card x${data.BirthdayBox}`)
+    itemToSell.push(`Birthday card x${data.BirthdayBox}`)
+    itemToSell.push(`Bottle of wine x${data.ClientGiftBox}`)
+    itemToSell.push(`Fruit basket x${data.ClientGiftBox}`)
+    itemToSell.push(`Pen x${data.ClientGiftBox}`)
+    console.log(itemToSell)
+    setDepartmentData(itemToSell)
+  }
+  
 
   return (
   <div className="App" >
@@ -102,13 +114,25 @@ function App() {
         {console.log("selectedDepartment "+selectedDepartment)}
           <div style={{ paddingTop: '20px' }}>
           <button
-            onClick={getData(selectedDepartment, selectedDate)}
+            onClick={handleGetDataClick}
             disabled={!selectedDepartment || !selectedDate || selectedDepartment === "Select Department" || selectedDate === "Select Date"}>
             Get Data
           </button>
           </div>
     </div>
 
+
+  <div style={{ marginTop: '50px' }}>
+     <table>
+      <tbody>
+        <td>
+          {departmentData.map((item, index) => (
+            <tr key={index}>{item}</tr>
+          ))}
+        </td>
+      </tbody>
+    </table>
+  </div>
 </div>
 
   
